7b8e98c8050a0b0ddba86dba4317bb6b0a9da89e
fepec <felipe.estrada.prada@gmail.com>
2023-08-30 11:31:58 -0500

    Add buttons and button js for all challenges

---
+++ js/script.js
@@ -0,0 +1,65 @@
+const btnChallengeOne = document.getElementById("btnChallenge1");
+const btnChallengeTwo = document.getElementById("btnChallenge2");
+const btnChallengeThree = document.getElementById("btnChallenge3");
+const btnChallengeFour = document.getElementById("btnChallenge4");
+const btnChallengeFive = document.getElementById("btnChallenge5");
+const btnChallengeSix = document.getElementById("btnChallenge6");
+const btnChallengeSeven = document.getElementById("btnChallenge7");
+const btnChallengeEight = document.getElementById("btnChallenge8");
+const btnChallengeNine = document.getElementById("btnChallenge9");
+
+function challengeUno() {
+  console.log("Ej1");
+}
+function challengeDos() {
+  console.log("Ej2");
+}
+function challengeTres() {
+  console.log("Ej3");
+}
+function challengeCuatro() {
+  console.log("Ej4");
+}
+function challengeCinco() {
+  console.log("Ej5");
+}
+function challengeSeis() {
+  console.log("Ej6");
+}
+function challengeSiete() {
+  console.log("Ej7");
+}
+function challengeOcho() {
+  console.log("Ej8");
+}
+function challengeNueve() {
+  console.log("Ej9");
+}
+
+btnChallengeOne.onclick = function () {
+  challengeUno();
+};
+btnChallengeTwo.onclick = function () {
+  challengeDos();
+};
+btnChallengeThree.onclick = function () {
+  challengeTres();
+};
+btnChallengeFour.onclick = function () {
+  challengeCuatro();
+};
+btnChallengeFive.onclick = function () {
+  challengeCinco();
+};
+btnChallengeSix.onclick = function () {
+  challengeSeis();
+};
+btnChallengeSeven.onclick = function () {
+  challengeSiete();
+};
+btnChallengeEight.onclick = function () {
+  challengeOcho();
+};
+btnChallengeNine.onclick = function () {
+  challengeNueve();
+};
